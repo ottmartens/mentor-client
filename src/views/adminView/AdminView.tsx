01907c5e30a8c4ed542f1a2bf11d741a41b58c88
eleriin <eleriin.r@gmail.com>
2019-12-08 14:11:59 +0200

    deadlines draft #115

--- src/views/adminView/AdminView.tsx
+++ src/views/adminView/AdminView.tsx
@@ -1,57 +1,68 @@
 import React, { useEffect } from 'react';
-import { Container, List, ListItem, Divider, Card, ListItemText, CardContent, FormControl, FormGroup} from '@material-ui/core';
+import {
+	Container,
+	List,
+	ListItem,
+	Divider,
+	Card,
+	ListItemText,
+	CardContent,
+	FormGroup,
+	Button,
+  FormControlLabel,
+  Checkbox,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { HasUserProps } from '../../types';
 import Loader from '../../components/loader/Loader';
 import useTranslator from '../../hooks/useTranslator';
 import { Translation } from '../../translations';
-import { UserContext } from '../../contexts/UserContext';
+//import { UserContext } from '../../contexts/UserContext';
 import Person from '../../components/person/Person';
 import useBackend, { RequestMethod, EndPoint } from '../../hooks/useBackend';
-import useInput, { UseInput } from '../../hooks/useInput';
-import CheckboxField from '../../components/checkboxField/CheckboxField';
 import { Link } from 'react-router-dom';
-
+import Notice from '../../components/notice/Notice';
+import { SentimentSatisfiedSharp } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
 	container: {
 		flexGrow: 1,
 		textAlign: 'center',
-        marginBottom: '16px',
-    },
-    listitem: {
-        display: 'flex',
-        flexGrow: 1,
-        flexDirection: 'row',
+		marginBottom: '16px',
+	},
+	listitem: {
+		display: 'flex',
+		flexGrow: 1,
+		flexDirection: 'row',
 		textDecoration: 'none',
 		color: 'initial',
-    },
-    link: {
+	},
+	link: {
 		textDecoration: 'none',
-        color: 'inherit',
-    },
-    title: {
-        color: '#2c4d7f'
-    },
-    menteeCard: {
+		color: 'inherit',
+	},
+	title: {
+		color: '#2c4d7f',
+	},
+	menteeCard: {
 		padding: '20px',
 		marginBottom: '12px',
 		marginTop: '20px',
-    },
-    personName: {
-        fontSize: '20px',
-    },
-    listImage: {
+	},
+	personName: {
+		fontSize: '20px',
+	},
+	listImage: {
 		borderRadius: '50%',
-    },
-    listitem2: {
-        display: 'flex',
-        flexGrow: 1,
-        flexDirection: 'row',
+	},
+	listitem2: {
+		display: 'flex',
+		flexGrow: 1,
+		flexDirection: 'row',
 		textDecoration: 'none',
 		color: 'initial',
-    },
-    acceptButton: {
+	},
+	acceptButton: {
 		margin: '4px',
 		backgroundColor: '#26c72b',
 		color: '#fff',
@@ -60,15 +71,14 @@ const useStyles = makeStyles((theme) => ({
 		margin: '4px',
 		backgroundColor: '#B40404',
 		color: '#fff',
-    },
-    cardTitle: {
-    },
-    suuredArvud: {
+	},
+	cardTitle: {},
+	suuredArvud: {
 		color: 'purple',
 		fontSize: '20px',
 		marginRight: '15px',
-    },
-    card: {
+	},
+	card: {
 		padding: '20px',
 		marginBottom: '12px',
 		marginTop: '20px',
@@ -78,6 +88,9 @@ const useStyles = makeStyles((theme) => ({
 		display: 'flex',
 		flexDirection: 'column',
 		alignItems: 'center',
+	},
+	addButton: {
+		margin: '1em auto',
 	},
 }));
 
@@ -91,142 +104,163 @@ interface Props extends HasUserProps {
 }
 
 export default function AdminView({ user }: HasUserProps) {
+	const classes = useStyles();
+	//const userContext = React.useContext(UserContext);
+	//const [added, isAdded] = React.useState(false);
+	const t = useTranslator();
+	const [hasChanged, setHasChanged] = React.useState(false);
 
-    const classes = useStyles();
-    //const userContext = React.useContext(UserContext);
-    //const [added, isAdded] = React.useState(false);
-    const t = useTranslator();
-     //const [hasChanged, setHasChanged] = React.useState(false);
-
-     const [state, setState] = React.useState({
-		mentor: false,
-		mentee: false,
-      });
-    
-    const [queryDeadlineData, { data:currentDeadlineData , /*loading:currentDeadlineLoading,*/ called:currentDeadlineCalled }] = useBackend({
+	const [ queryDeadlineData, { data: currentDeadlineData, loading: currentDeadlineLoading, called: currentDeadlineCalled },] = useBackend({
 		requestMethod: RequestMethod.GET,
-		endPoint: EndPoint.DEADLINE,
+		endPoint: EndPoint.SETTINGS,
 		authToken: user.token,
-    });
+  });
+  
+	const [state, setState] = React.useState({
+		mentor: (currentDeadlineData ? currentDeadlineData.MENTORS_CAN_REGISTER : false),
+		mentee: (currentDeadlineData ? currentDeadlineData.MENTEES_CAN_REGISTER : false),
+	});
 
-    const input: { [s: string]: UseInput } = {
-		mentor: useInput({ initialValue: (currentDeadlineData && currentDeadlineData.mentor) || '' }),
-		mentee: useInput({ initialValue: (currentDeadlineData && currentDeadlineData.mentee) || '' }),
-    };
-    const [queryUnverifiedActivities, { data:activityData, loading:activityLoading, called:activityCalled }] = useBackend({
+	const [
+		queryUnverifiedActivities,
+		{ data: activityData, loading: activityLoading, called: activityCalled },
+	] = useBackend({
 		requestMethod: RequestMethod.GET,
 		endPoint: EndPoint.UNVERIFIED_ACTIVITIES,
 		authToken: user.token,
-    });
-    
-    const [queryAllUsers, { data:usersData, loading:usersLoading, called:usersCalled }] = useBackend({
+	});
+
+	const [queryAllUsers, { data: usersData, loading: usersLoading, called: usersCalled }] = useBackend({
 		requestMethod: RequestMethod.GET,
 		endPoint: EndPoint.ALL_USERS,
 		authToken: user.token,
-    });
-    
-	const [changeDeadlinesFn, { error }] = useBackend({
+	});
+
+	const [changeDeadlinesFn, { called, error }] = useBackend({
 		requestMethod: RequestMethod.POST,
-        endPoint: EndPoint.CHANGE_DEADLINES_REQUEST,
-        variables: {
-			mentor: input.mentor.value,
-			mentee: input.mentee.value
+		endPoint: EndPoint.SETTINGS,
+		variables: {
+			MENTORS_CAN_REGISTER: state.mentor.value,
+			MENTEES_CAN_REGISTER: state.mentee.value,
 		},
 		authToken: user.token,
-    });
-    
-	   
-    React.useEffect(() => {
+	});
+
+  const setStates = () => {
+    setState({ ...state, ['mentor']: currentDeadlineData ? currentDeadlineData.MENTORS_CAN_REGISTER : false});
+    setState({ ...state, ['mentee']: currentDeadlineData ? currentDeadlineData.MENTEES_CAN_REGISTER : false});
+  }
+
+  React.useEffect(() => {
 		if (activityCalled) {
-            console.log(activityData)
+//			console.log(activityData);
 			return;
 		}
 		queryUnverifiedActivities();
-    }, [activityCalled, queryUnverifiedActivities]);
-    
-
-    React.useEffect(() => {
+  }, [activityCalled, queryUnverifiedActivities]);
+  
+	React.useEffect(() => {
 		if (usersCalled) {
 			return;
 		}
 		queryAllUsers();
-    }, [usersCalled, queryAllUsers]);
-    
+	}, [usersCalled, queryAllUsers]);
 
-   /*React.useEffect(() => {
-    if (currentDeadlineCalled) {
-        return;
-    }
+	React.useEffect(() => {
+		if (currentDeadlineCalled) {
+			return;
+		}
     queryDeadlineData();
-    }, [currentDeadlineCalled, queryDeadlineData]);*/
-    
-    
-    if (activityLoading || !usersData || usersLoading /*|| currentDeadlineLoading*/ || !activityData /*|| !currentDeadlineData*/) {
-		return <Loader />;
-    }
-    
+    setStates();
+	}, [currentDeadlineCalled, queryDeadlineData]);
 
-    const activitytotal = activityData && activityData.length;
-    const usertotal = usersData && usersData.length;
-	    
-    return (
-        <Container className={classes.container} maxWidth="sm">
-             {/*{error && <Notice variant="error" title="Tähtaegade muutmine ebaõnnestus" message={error} />}
-			{hasChanged && <Notice variant="success" title="Tähtajad muudetud" message=''/>}*/}
-            <h1 className={classes.title}>{t(Translation.ADMIN_OVERVIEW)}</h1>
-            <Card className={classes.menteeCard}>
-                <h2 className={classes.cardTitle}>{t(Translation.GRADE_ACTIVITIES)}</h2>
-                <h3>
-                    {t(Translation.ADMIN_UNVERIFIED_ACTIVITIES)}: <span className={classes.suuredArvud}>{activitytotal}</span>
-                </h3>
-            <List>
-                {activityData && activityData.map(({name, groupName, ID}) => {
-                    return <div key={ID}>
-                        <ListItem className={classes.listitem}>
-                            <div>
-                                <Link
-                                to={ID ? `/activities/activity/${ID}` : '#'}
-                                className={classes.link}
-                                >
-                                    <ListItemText className={classes.personName} primary={name} secondary={groupName || t(Translation.NAMELESS_GROUP)}/>
-                                </Link>
-                            </div>
-                        </ListItem>
-                        <Divider variant="middle"/>
-                    </div>
-                })}
-            </List>
-            </Card>
-            <div>
-            <Card className={classes.menteeCard}>
-                <h2 className={classes.cardTitle}>{t(Translation.VERIFY_USERS)}</h2>
-                <h3>
-                    {t(Translation.ADMIN_UNVERIFIED_USERS)}: <span className={classes.suuredArvud}>{usertotal}</span>
-                </h3>
-                <List>
-                    {usersData && usersData.map(({ID, name, tagline, imageurl}) => {
-                    return <div key={ID}>
-                        <Person name={name} tagline={tagline} imageUrl={imageurl} userId={ID}></Person>
-                    </div>
-                })}
-                </List>
-            </Card>
-            </div>
+	if (
+		activityLoading ||
+		!usersData ||
+		usersLoading ||
+		currentDeadlineLoading ||
+		!activityData ||
+		!currentDeadlineData
+	) {
+		return <Loader />;
+	}
+
+	const activitytotal = activityData && activityData.length;
+	const usertotal = usersData && usersData.length;
+  
+	return (
+		<Container className={classes.container} maxWidth="sm">
+			{error && <Notice variant="error" title="Tähtaegade muutmine ebaõnnestus" message={error} />}
+			{hasChanged && <Notice variant="success" title="Tähtajad muudetud" message="" />}
+			<h1 className={classes.title}>{t(Translation.ADMIN_OVERVIEW)}</h1>
+			<Card className={classes.menteeCard}>
+				<h2 className={classes.cardTitle}>{t(Translation.GRADE_ACTIVITIES)}</h2>
+				<h3>
+					{t(Translation.ADMIN_UNVERIFIED_ACTIVITIES)}: <span className={classes.suuredArvud}>{activitytotal}</span>
+				</h3>
+				<List>
+					{activityData &&
+						activityData.map(({ name, groupName, ID }) => {
+							return (
+								<div key={ID}>
+									<ListItem className={classes.listitem}>
+										<div>
+											<Link to={ID ? `/activities/activity/${ID}` : '#'} className={classes.link}>
+												<ListItemText
+													className={classes.personName}
+													primary={name}
+													secondary={groupName || t(Translation.NAMELESS_GROUP)}
+												/>
+											</Link>
+										</div>
+									</ListItem>
+									<Divider variant="middle" />
+								</div>
+							);
+						})}
+				</List>
+			</Card>
+			<div>
+				<Card className={classes.menteeCard}>
+					<h2 className={classes.cardTitle}>{t(Translation.VERIFY_USERS)}</h2>
+					<h3>
+						{t(Translation.ADMIN_UNVERIFIED_USERS)}: <span className={classes.suuredArvud}>{usertotal}</span>
+					</h3>
+					<List>
+						{usersData &&
+							usersData.map(({ ID, name, tagline, imageurl }) => {
+								return (
+									<div key={ID}>
+										<Person name={name} tagline={tagline} imageUrl={imageurl} userId={ID}></Person>
+									</div>
+								);
+							})}
+					</List>
+				</Card>
+			</div>
 			<div className={classes.container}>
-                <Card className={classes.card}>
-                    <CardContent>
-						<FormControl component="fieldset">
+				<Card className={classes.card}>
+					<CardContent>
+						<form
+            onClick={async (e) => {
+              e.preventDefault();
+              await changeDeadlinesFn();
+              await queryDeadlineData();
+              !error && setHasChanged(true);
+              console.log('mentor: ' + state.mentor.value + ', mentee: ' + state.mentee.value)
+						}}
+						>
 							<h1>{t(Translation.DEADLINES)}</h1>
-							<FormGroup>
-								<CheckboxField label='Registreerimine avatud mentoritele' {...input.mentor} value={true}></CheckboxField>
-									{/*value={data.mentor}>*/}
-								<CheckboxField label='Registreerimine avatud menteedele' {...input.mentor} value={false}></CheckboxField>
-									{/*value={data.mentee}>*/}
-							</FormGroup>
-						</FormControl>
-                    </CardContent>
-                </Card>
-            </div>
-        </Container>
+							<div className={classes.buttons}>
+                <FormGroup>
+                  <FormControlLabel checked={state.mentor.value} value={state.mentor.value} control={<Checkbox onChange={() => setState({ ...state, ['mentor']: !state.mentor.value})} />} label={'Regstreerimine avatud mentoritele'} />
+                  <FormControlLabel checked={state.mentee.value} value={state.mentee.value} control={<Checkbox onChange={() => setState({ ...state, ['mentee']: !state.mentee.value})} />} label={'Regstreerimine avatud menteedele'} />
+							  </FormGroup>
+              </div>
+						</form>
+					</CardContent>
+				</Card>
+			</div>
+		</Container>
 	);
 }
